ebbcc835794efd6b62f08eae2fc0d9b537887860
gernest <geofreyernest@live.com>
2015-05-06 13:36:04 +0300

    added profile update

--- public/js/aurora.js
+++ public/js/aurora.js
@@ -4,14 +4,39 @@
 
 $(document).ready(function(){
     $('.button-collapse').sideNav();
-
     // profile
-    profileUpdateBtn=$('.update-btn');
-    profileUpdateSect=$('.update-section');
-    profileUpdateBtn.click(function(){
-        profileUpdateSect.toggle();
-    })
+    var profileUpdate=function(){
+        var upBtn=$('.up-btn');
+        var upSection=$('.up-section');
+        var upProgress=$('.up-progress');
+        var upError=$('.up-error');
+        var upForm=$('.up-form');
+        var upMsg=$('.up-message')
 
+        upSection.toggle();
+        upProgress.toggle();
+        upBtn.click(function(){ upSection.toggle()});
+        upForm.submit(function(e){
+            upForm.toggle();
+            upProgress.toggle();
+            $.ajax({
+                url:$(this).attr('action'),
+                type:$(this).attr('method'),
+                data:$(this).serialize(),
+                success:function(data){
+                    upProgress.toggle();
+                    upMsg.html(data);
+                },
+                error:function(res,status,err){
+                    upProgress.toggle();
+                    upMsg.html(res.responseText);
+                    upForm.toggle();
+                }
+            })
+            e.preventDefault();
+            e.unbind
+        });
 
-
+    };
+    profileUpdate();
 });
