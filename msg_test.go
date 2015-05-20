37297d84a6734571cfb6062bb752e7fb1ea285ea
gernest <geofreyernest@live.com>
2015-05-20 16:34:21 +0300

    added test for mesenger

--- msg_test.go
+++ msg_test.go
@@ -1,18 +1,25 @@
 package aurora
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 	"testing"
+	"time"
 
 	"golang.org/x/net/websocket"
 )
 
 func TestMessenger(t *testing.T) {
-	var email = "wesucks@aurora.com"
-	var pass = "mamamia"
-	var loginPath = "/auth/login"
+	var (
+		email     = "wesuckssoomuch@aurora.com"
+		pass      = "mamamia"
+		loginPath = "/auth/login"
+		userID    = "37c37153-089e-4c19-466e-2f467ac07c1e"
+	)
 
 	ts, client, rx := testServer(t)
 	defer ts.Close()
@@ -30,18 +37,9 @@ func TestMessenger(t *testing.T) {
 		t.Error(err)
 	}
 
-	// There is no session yet, it should fail to validate
-	ws, err := websocket.DialConfig(cfg)
-	if err == nil {
-		t.Error("Expected an error")
-	}
-	if ws != nil {
-		t.Error("Expected nil")
-	}
-
 	// create a user and profile
 	usr := &User{
-		UUID:         getUUID(),
+		UUID:         userID,
 		EmailAddress: email,
 	}
 	ps, err := hashPassword(pass)
@@ -51,7 +49,7 @@ func TestMessenger(t *testing.T) {
 	usr.Pass = ps
 	err = CreateAccount(setDB(rx.db, rx.cfg.AccountsDB), usr, rx.cfg.AccountsBucket)
 	if err != nil {
-		t.Error(err)
+		t.Errorf("creating a new account %v", err)
 	}
 	pdbStr := getProfileDatabase(rx.cfg.DBDir, usr.UUID, rx.cfg.DBExtension)
 	pdb := setDB(rx.db, pdbStr)
@@ -60,11 +58,23 @@ func TestMessenger(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+
+	// There is no session yet, it should fail to validate
+	ws1, err := websocket.DialConfig(cfg)
+	if err == nil {
+		t.Error("Expected an error")
+	}
+	if ws1 != nil {
+		t.Error("Expected nil")
+		ws1.Close()
+	}
+
 	// login and create a session for user with pids[0]
 	varsLogin := url.Values{
 		"email":    {email},
 		"password": {pass},
 	}
+
 	res9, err := client.PostForm(fmt.Sprintf("%s%s", ts.URL, loginPath), varsLogin)
 	if err != nil {
 		t.Error(err)
@@ -84,9 +94,47 @@ func TestMessenger(t *testing.T) {
 	}
 	cfg2.Header = h
 
-	ws, err = websocket.DialConfig(cfg2)
+	ws2, err := websocket.DialConfig(cfg2)
 	if err != nil {
 		t.Error(err)
 	}
-	ws.Close()
+	defer ws2.Close()
+
+	// try sending a bad request
+	tmsg := &MSG{
+		SenderID: getUUID(),
+		Text:     "hellp gernest",
+	}
+	d, err := marshalAndPach("send", tmsg)
+	if err != nil {
+		t.Errorf("marshaling and packing %v")
+	}
+
+	err = ws2.SetWriteDeadline(time.Now().Add(time.Second))
+	if err != nil {
+		t.Errorf("set write deadline %v", err)
+	}
+	_, err = ws2.Write(d)
+	if err != nil {
+		t.Errorf("writing message %v", err)
+	}
+}
+
+func marshalAndPach(name string, dPtr interface{}) ([]byte, error) {
+	var protocolSeperator = " "
+	if data, err := json.Marshal(dPtr); err == nil {
+		result := []byte(name + protocolSeperator)
+		return append(result, data...), nil
+	} else {
+		return nil, err
+	}
+}
+
+func unpackMSG(data []byte) (string, interface{}, error) {
+	var protocolSeperator = " "
+	result := strings.SplitN(string(data), protocolSeperator, 2)
+	if len(result) != 2 {
+		return "", nil, errors.New("Unable to extract event name from data.")
+	}
+	return result[0], []byte(result[1]), nil
 }
